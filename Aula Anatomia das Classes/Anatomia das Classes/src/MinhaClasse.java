8181eb24edf615ee9e103f5bfc87e3daef34593a
Junior Martins <claudineijunior1996@gmail.com>
2024-05-12 22:07:20 -0300

    MANDAR SYNC

---
+++ Aula Anatomia das Classes/Anatomia das Classes/src/MinhaClasse.java
@@ -0,0 +1,20 @@
+public class MinhaClasse {
+    
+    public static void main(String[] args) throws Exception {
+       
+        final String BR = "Brasil"; // A variavel nunca vai mudar o valor (final = não deixa alterar)
+        double PI = 3.14; // Tipo de valor float
+        int idade = 20; // variavel de valor inteira
+        int anoFabricacao = 2024; // pode ser usado assim
+        boolean verdadeiro = true; // valor bool 1 ou 0 
+        String primeiroNome = "Claudinei"; //atribuindo primeiro nome
+        String segundoNome = "Júnior"; // atribuindo segundo nome
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome); //chamada do método
+        //quando chegar nesse momento vai chamar o método nomeCompleto 
+
+        System.out.println(nomeCompleto); // aqui vai aparecer o return do Método
+        } //fecha o main
+    public static String nomeCompleto (String primeiroNome,String segundoNome){ // criando o método
+        return "Resultado do Método "+ primeiroNome.concat(" ").concat(segundoNome); //Concatena os dois nomes
+    } //fecha o nomeCompleto
+} // fecha a classe MinhaClasse
--- sEGUNDO TESTE/Meu primeiro projeto java/src/App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Sr. Martins !!! deu certo o projeto no GitHub");
-    }
-}
